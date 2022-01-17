cb7c37bd472ff215115d0100c6e53641db9ba019
Greg Chlosta <grzegorz.chlosta@gmail.com>
2022-01-16 17:36:50 -0700

    added google analitics

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp, getApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyB5GupZHFnG8rd9aiACplJ73JNRXRMWHSU',
@@ -8,9 +9,11 @@ const firebaseConfig = {
   storageBucket: 'next-portfolio-b90b7.appspot.com',
   messagingSenderId: '139741735429',
   appId: '1:139741735429:web:b69485f845452b37458a6c',
+  measurementId: 'G-7633YVGBT3',
 }
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 const db = getFirestore()
+const analytics = getAnalytics(app)
 
-export { app, db }
+export { app, db, analytics }
