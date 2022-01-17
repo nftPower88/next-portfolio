b3ae96dc8331729fcb61c2d6e73ee7d313985fa8
Greg Chlosta <grzegorz.chlosta@gmail.com>
2022-01-16 20:09:53 -0700

    Snake Title fix

--- components/PlaySnake.js
+++ components/PlaySnake.js
@@ -7,10 +7,11 @@ export default function PlaySnake() {
       <div className='max-w-6xl mx-auto px-5'>
         <header className='flex flex-col md:flex-row pt-20 lg:pt-36 mx-10 md:my-20 lg:my-0 lg:mb-5'>
           <span className='flex flex-row items-end'>
+            <p className='text-4xl mr-3 mb-2'>or</p>
             <h1 className='text-6xl lg:text-9xl md:max-w-4xl font-bold text-gray-800'>
               Play Snake!
             </h1>
-            <p className='text-2xl ml-3 mb-2'>and relax ☺️</p>
+            <p className='text-4xl ml-3 mb-2'>and relax...</p>
           </span>
         </header>
 
